157ef5107b7e2170f75d14ab72dd81af3c118538
Ezekiel Compton <compton.ezekiel1@gmail.com>
2022-07-23 13:32:39 -0500

    Added simple ui to rock paper scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,103 +1,115 @@
- function computerPlay(){
+let x=0
+cScore=0
+pScore=0 
+let result= document.querySelector('.result')
+let playerScore=document.querySelector('.p-count')
+let computerScore=document.querySelector(".c-count")
+let userChoice="" 
+let btns= document.querySelectorAll('button');
+
+
+for (i of btns){
+    i.addEventListener('click', function(){
+        userChoice=event.target.value
+        console.log(userChoice)
+       gameOver()
+        game()
+       
+    })
+
+}
+function computerPlay(){
     const computerChoice= Math.floor(Math.random()*3)
     switch (computerChoice){
         case 0:
             x= "paper"
-            break;
+            return x;
         case 1:
              x= "scissors"
-             break;
+             return x;
         case 2:
             x= "rock"
-        
-        break;
+        console.log(x)
+        return x    }}
+
     
-     
-    }
-
-       
-    
-}
-let n=1
-let y= 1
-let i=1
-
-
-
-
-
-
-
-function game (){
-  
-    const promptVar= prompt("choose rock paper or scissors")
-    const userChoice= promptVar.toLowerCase()
-    
+   
+function game(){
+ 
     computerPlay()
-    
-    
-     if
-        (userChoice=="rock" && x=="scissors")
-            {n++
-            i++
-            }
-    else if
-         (x=="rock" && userChoice=="scissors")
-            {
-                y++
-            i++}
-    else if
-         (userChoice=="paper" && x=="scissors")
-            {
-            y++
-            i++}
-    else if
-         (x=="paper" && userChoice=="scissors")
-            {
-                n++
-            i++}
-    else if        
-     (x=="rock" && userChoice=="paper")
-            {
-            n++
-            i++}
-            
-    else if
-    (x=="paper" && userChoice=="rock")
-            {
-                y++
-           i++}
-    else 
-       { alert("Draw! Try again!")
-        game()}
-    
+   
+   
+   
 
-    if(i==5 && (y>n)){
-        alert("Thats game! Computer wins!")
+    
+    if( userChoice=="rock" && x=="paper"){
+       
+        ++cScore
         
+        computerScore.innerHTML=cScore
+        result.innerHTML=x
+        }
+    
+    else if ( userChoice=="rock" && x=="scissors"){
+    
+        
+        ++pScore
+        playerScore.innerHTML=pScore
+        result.innerHTML=x}
+    
+    else if ( userChoice=="paper" && x=="rock"){
+       
+            ++pScore
+        playerScore.innerHTML=pScore
+            result.textContent=x
+            }
+
+    else if ( userChoice=="paper" && x=="scissors"){
+       
+                ++cScore
+                console.log(cScore)
+                computerScore.innerHTML=cScore
+                result.innerHTML=x
+              }
+    
+    else if ( userChoice=="scissors" && x=="rock"){
+       
+                    ++cScore
+                    console.log(cScore)
+                    computerScore.innerHTML=cScore
+                    result.innerHTML=x
+                    }
+    else if ( userChoice=="scissors" && x=="paper"){
+                       
+                        ++pScore
+                        playerScore.innerHTML=pScore
+                        result.innerHTML=x
+                        
+                     
     }
-
-    else if (i==5 && (n>y)){
-alert("Thats game! You win!")
-    }
-
-    else if (i<5)
-    game()
-    console.log(i)
-    console.log(n)
-    console.log(y)
-
-
+else if
+    (userChoice==x){
+        result.textContent="Thats a tie try again!"
+}
 
 }
-        
+
+function gameOver(){
+    if (cScore==5){
+        alert("I win! Play again?")
+        location.reload()
+    }
+
+    else if (pScore==5){
+        alert("You win! Play again?")
+       location.reload()
+    }
+
+}
+    
+
+  
 
 
 
 
-
-
-
-game(
-
-)
